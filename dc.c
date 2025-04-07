9d26a3de44a18ecab18294b43de09f4d6e65c613
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-04-07 14:29:20 -0400

    add the signal handling  for the Sigint

--- dc.c
+++ dc.c
@@ -1,20 +1,21 @@
-// hitogram.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
-#include <iostream>
+pid_t dp1_pid = 0;
+pid_t dp2_pid = 0;
+shared_memory_t* shm_ptr = NULL;
+int shm_id = 0;
+int sem_id = 0;
+int counts[NUM_LETTERS] = { 0 };
+int should_exit = 0;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void signal_handler(int signum) {
+    if (signum == SIGINT) {
+        should_exit = 1;
+
+        kill(dp1_pid, SIGINT);
+        kill(dp2_pid, SIGINT);
+    }
+    else if (signum == SIGALRM) {
+
+        alarm(2);
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
