06db6feed2c61d08442e1e8cafd97b708cf21f03
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-04-07 20:20:17 -0400

    create a functions for shared memory and semaphore management and chake the conditions for the failed or not

--- common.c
+++ common.c
@@ -17,3 +17,50 @@ void initialize_shared_memory(shared_memory_t** shm_ptr, int* shm_id) {
     (*shm_ptr)->read_index = 0;
     (*shm_ptr)->write_index = 0;
 }
+
+
+void cleanup_shared_memory(int shm_id, shared_memory_t* shm_ptr) {
+
+    if (shmdt(shm_ptr) == -1) {
+        perror("shmdt failed");
+    }
+
+
+    if (shmctl(shm_id, IPC_RMID, NULL) == -1) {
+        perror("shmctl failed");
+    }
+}
+
+void initialize_semaphore(int* sem_id) {
+
+    *sem_id = semget(SEM_KEY, 1, IPC_CREAT | 0666);
+    if (*sem_id == -1) {
+        perror("semget failed");
+        exit(1);
+    }
+
+
+    union semun {
+        int val;
+        struct semid_ds* buf;
+        unsigned short* array;
+    } arg;
+    arg.val = 1;
+    if (semctl(*sem_id, 0, SETVAL, arg) == -1) {
+        perror("semctl failed");
+        exit(1);
+    }
+}
+
+void cleanup_semaphore(int sem_id) {
+    if (semctl(sem_id, 0, IPC_RMID) == -1) {
+        perror("semctl failed");
+    }
+}
+
+void wait_semaphore(int sem_id) {
+    struct sembuf sb = { 0, -1, 0 };
+    if (semop(sem_id, &sb, 1) == -1) {
+        perror("semop wait failed");
+    }
+}
