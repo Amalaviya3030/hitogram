6e15ee9c0ad4b0c1eeef2cd16511fa800ba1d96f
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-04-07 22:01:52 -0400

    Implement main loop in dc to read from shared memory and update histogram

--- dc.c
+++ dc.c
@@ -25,4 +25,55 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+
+    dp2_pid = atoi(argv[1]);
+    dp1_pid = atoi(argv[2]);
+    shm_id = atoi(argv[3]);
+
+
+    signal(SIGINT, signal_handler);
+    signal(SIGALRM, signal_handler);
+
+
+    shm_ptr = (shared_memory_t*)shmat(shm_id, NULL, 0);
+    if (shm_ptr == (void*)-1) {
+        perror("shmat failed");
+        exit(1);
+    }
+
+
+    sem_id = semget(SEM_KEY, 1, 0);
+    if (sem_id == -1) {
+        perror("semget failed");
+        exit(1);
+    }
+
+
+    alarm(2);
+
+
+    while (!should_exit) {
+        wait_semaphore(sem_id);
+
+
+        while (shm_ptr->read_index != shm_ptr->write_index) {
+            char letter = shm_ptr->buffer[shm_ptr->read_index];
+            counts[letter - 'A']++;
+            shm_ptr->read_index = (shm_ptr->read_index + 1) % BUFFER_SIZE;
+        }
+
+        signal_semaphore(sem_id);
+        pause();
+    }
+
+
+    display_histogram(counts);
+
+
+    if (shm_ptr != NULL) {
+        cleanup_shared_memory(shm_id, shm_ptr);
+    }
+
+    printf("Shazam !!\n");
+    return 0;
 }
